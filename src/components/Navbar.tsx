cc5b61aad61166439f850557a84ac9ba0150c45a
Vespers Makado <vespersmakadogmail.com>
2025-10-04 00:52:43 +0200

    Update Navbar with responsive hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+// src/components/Navbar.tsx
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">🌿 TranquilMind</div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/lessons">Lessons</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
+
+      {/* Hamburger icon */}
+      <div 
+        className="menu-toggle" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      {/* Nav links */}
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/lessons" onClick={() => setMenuOpen(false)}>Lessons</Link></li>
+        <li><Link to="/progress" onClick={() => setMenuOpen(false)}>Progress</Link></li>
+        <li><Link to="/login" onClick={() => setMenuOpen(false)}>Login</Link></li>
+        <li><Link to="/register" onClick={() => setMenuOpen(false)}>Register</Link></li>
       </ul>
     </nav>
   );
