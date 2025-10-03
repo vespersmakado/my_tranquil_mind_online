29f46d4147f7e421fb4aa861078264b81dd6d53e
Vespers Makado <vespersmakadogmail.com>
2025-10-04 01:05:58 +0200

    Remove old layout Navbar and switch to components/Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./layout/Navbar";
+import Navbar from "./components/Navbar";
 import Footer from "./layout/Footer";
 
 import Progress from "./pages/Progress";
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,25 @@
-// src/components/Navbar.tsx
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar: React.FC = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      <div className="logo">🌿 TranquilMind</div>
-
-      {/* Hamburger icon */}
-      <div 
-        className="menu-toggle" 
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        ☰
+      <div className="nav-container">
+        <div className="logo">🌿 TranquilMind</div>
+        <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+          ☰
+        </div>
+        <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+          <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+          <li><Link to="/lessons" onClick={() => setIsOpen(false)}>Lessons</Link></li>
+          <li><Link to="/progress" onClick={() => setIsOpen(false)}>Progress</Link></li>
+          <li><Link to="/login" onClick={() => setIsOpen(false)}>Login</Link></li>
+          <li><Link to="/register" onClick={() => setIsOpen(false)}>Register</Link></li>
+        </ul>
       </div>
-
-      {/* Nav links */}
-      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-        <li><Link to="/lessons" onClick={() => setMenuOpen(false)}>Lessons</Link></li>
-        <li><Link to="/progress" onClick={() => setMenuOpen(false)}>Progress</Link></li>
-        <li><Link to="/login" onClick={() => setMenuOpen(false)}>Login</Link></li>
-        <li><Link to="/register" onClick={() => setMenuOpen(false)}>Register</Link></li>
-      </ul>
     </nav>
   );
 };
