3c60b5443cb6a5656bfc3fa9beeb684e47e5ad48
Vespers Makado <vespersmakadogmail.com>
2025-10-04 01:26:13 +0200

    Replace old Navbar with new responsive version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,19 +5,27 @@ import "./Navbar.css";
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="nav-container">
-        <div className="logo">🌿 TranquilMind</div>
-        <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+      <div className="navbar-container">
+        {/* Logo left */}
+        <div className="logo">🌿 MyTranquilMind</div>
+
+        {/* Hamburger toggle (mobile) */}
+        <div className="menu-toggle" onClick={toggleMenu}>
           ☰
         </div>
+
+        {/* Links right */}
         <ul className={`nav-links ${isOpen ? "active" : ""}`}>
-          <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
-          <li><Link to="/lessons" onClick={() => setIsOpen(false)}>Lessons</Link></li>
-          <li><Link to="/progress" onClick={() => setIsOpen(false)}>Progress</Link></li>
-          <li><Link to="/login" onClick={() => setIsOpen(false)}>Login</Link></li>
-          <li><Link to="/register" onClick={() => setIsOpen(false)}>Register</Link></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/lessons">Lessons</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register" className="btn">Sign Up</Link></li>
         </ul>
       </div>
     </nav>
